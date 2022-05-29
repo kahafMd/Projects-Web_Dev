f0e2a6c11b9ed220f1e8216945f01809b32bac87
Kahaf <modkf99@gmail.com>
2022-05-29 10:15:31 +0530

    Countdown Timer

---
+++ Countdown Timer/script.js
@@ -0,0 +1,31 @@
+
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const currentYear = new Date().getFullYear();
+// console.log(currentYear)
+const newYearTime = new Date ( `Jan 1 ${currentYear + 1 } 00:00:00 `);
+// console.log(newYearTime)
+
+
+// Update Countdowntime
+function updateCountdowntime() {
+    const currentTime = new Date();
+    const diff = newYearTime - currentTime;
+
+    const d = Math.floor(diff/1000/60/60/24);
+    const h = Math.floor(diff/1000/60/60)%24;
+    const m = Math.floor(diff/1000/60)%60;
+    const s = Math.floor(diff/1000)%60;
+
+days.innerHTML = d ;
+hours.innerHTML = h < 10 ? '0' +  h : h;
+minutes.innerHTML = m < 10 ? '0' +  m : m;
+seconds.innerHTML = s < 10 ? '0' +  s : s;
+
+
+}
+
+setInterval( updateCountdowntime , 1000);
