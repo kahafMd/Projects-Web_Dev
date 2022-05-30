d63abdaf4c25008a814424c75904281fee9cc8f4
Kahaf <modkf99@gmail.com>
2022-05-30 13:02:26 +0530

    Dark Mode Added

--- Online Calculator/script.js
+++ Online Calculator/script.js
@@ -1,22 +1,31 @@
+const checkbox = document.getElementById('checkbox');
 let string = "";
 let buttons = document.querySelectorAll('.button');
+
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click',(e)=>{
         if(e.target.innerHTML == '='){
             string = eval(string);
-            document.querySelector('input').value = string;
+            document.querySelector('.input').value = string;
         }
         else if(e.target.innerHTML == 'C'){
             string = "";
-            document.querySelector('input').value = string;
+            document.querySelector('.input').value = string;
         }
         else{
             
             console.log(e.target)
             string = string + e.target.innerHTML;
-            document.querySelector('input').value = string;
+            document.querySelector('.input').value = string;
         }
     })
 })
 
-         
+
+checkbox.addEventListener('change',()=>{
+    // Changing the theme of our website
+
+    document.body.classList.toggle('dark');
+    // alert("Dark Mode ON/OFF")
+
+})         
